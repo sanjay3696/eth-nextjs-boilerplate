e48145b861af422e85613b6a9302859ec2dd06dc
sanjay3696 <66181363+sanjay3696@users.noreply.github.com>
2022-11-22 01:30:31 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,15 @@ import { BigNumber } from '@ethersproject/bignumber'
 
 const Home: NextPage = () => {
 
-  const { data, isIdle, isError, isLoading, isSuccess, sendTransaction } =
-    useSendTransaction({
-      return: {
-        <form>
+  export function SendTransaction() {
+  return (
+    <form>
       <input aria-label="Recipient" placeholder="0xA0Cf…251e" />
       <input aria-label="Amount (ether)" placeholder="0.05" />
       <button>Send</button>
     </form>
-      },
-    })
+  )
+}
 
   return (
     <div className={styles.container}>
